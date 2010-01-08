029cf60b1b361ca301a511d1996ff0a872c87c75
Daniel Salinas <imsplitbit@gmail.com>
2010-01-08 00:18:16 -0600

    updating old makefile to my new template

--- tiny_usb_demo/firmware/main.c
+++ tiny_usb_demo/firmware/main.c
@@ -4,60 +4,60 @@
  * License: GPL v3
  */
 
-#include <avr/io.h>			// default
-#include <avr/interrupt.h>	// for sei()
-#include <avr/pgmspace.h>	// required by usbdrv.h
-#include <avr/wdt.h>		// Needed for usb stuffs
-#include <util/delay.h>		// for _delay_ms()
+#include <avr/io.h>                     // default
+#include <avr/interrupt.h>      // for sei()
+#include <avr/pgmspace.h>       // required by usbdrv.h
+#include <avr/wdt.h>            // Needed for usb stuffs
+#include <util/delay.h>         // for _delay_ms()
 
-#include "usbdrv.h"
+#include "usbconfig.h"
 
 void ledOn()
 {
-	PORTD |= 1 << PD3;
+        PORTD |= 1 << PD3;
 }
 
 void ledOff()
 {
-	PORTD &= 0 << PD3;
+        PORTD &= 0 << PD3;
 }
 
 uchar usbFunctionSetup(uchar data[8])
 {
-	switch (data[1])
-	{
-		case 0:
-			ledOn();
-			break;
-		case 1:
-			ledOff();
-			break;
-		default:
-			break;
-	}
-	return 0;
+        switch (data[1])
+        {
+                case 0:
+                        ledOn();
+                        break;
+                case 1:
+                        ledOff();
+                        break;
+                default:
+                        break;
+        }
+        return 0;
 }
 
 int main(void)
-{	
-	PORTD = 0;
-	DDRD |= 1 << PD3;
-	DDRD &= 0 << PD2;
-	DDRD &= 0 << PD5;
-	
-	wdt_enable(WDTO_1S);
-	
-	usbInit();
-	usbDeviceDisconnect();
-	
-	_delay_ms(250);
-	
-	usbDeviceConnect();
-	sei();
-	
+{
+        PORTD = 0;
+        DDRD |= 1 << PD3;
+        DDRD &= 0 << PD2;
+        DDRD &= 0 << PD5;
+
+        wdt_enable(WDTO_1S);
+
+        usbInit();
+        usbDeviceDisconnect();
+
+        _delay_ms(250);
+
+        usbDeviceConnect();
+        sei();
+
     for(;;){
-		wdt_reset();
-		usbPoll();
+                wdt_reset();
+                usbPoll();
     }
     return 0;
 }
